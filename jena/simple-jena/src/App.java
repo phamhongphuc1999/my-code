22f0d21735b05f7cbd39c6d1a6fc88a701d0c908
phamhongphuc1999 <phamhongphuc12atm1@gmail.com>
2023-08-31 20:06:27 +0700

    feat: reasoner

--- jena/simple-jena/src/App.java
+++ jena/simple-jena/src/App.java
@@ -1,7 +1,17 @@
+import org.apache.jena.ontology.OntModel;
+import org.apache.jena.ontology.OntModelSpec;
+import org.apache.jena.rdf.model.InfModel;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.rdf.model.Property;
 import org.apache.jena.rdf.model.ResIterator;
 import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.rdf.model.Statement;
+import org.apache.jena.rdf.model.StmtIterator;
+import org.apache.jena.reasoner.Reasoner;
+import org.apache.jena.reasoner.ReasonerRegistry;
+import org.apache.jena.riot.RDFDataMgr;
+import org.apache.jena.util.PrintUtil;
 import org.apache.jena.vocabulary.VCARD;
 
 public class App {
@@ -13,7 +23,7 @@ public class App {
 
         Resource animal1 = m.createResource(URL1);
         animal1.addProperty(VCARD.FN, "Elephant");
-        
+
         Resource animal2 = m.createResource(URL2);
         animal2.addProperty(VCARD.FN, "Dog");
 
@@ -23,9 +33,43 @@ public class App {
         }
         m.write(System.out, "Turtle");
     }
+
+    public static void load() {
+        Model m = ModelFactory.createDefaultModel();
+        // read into the model.
+        m.read("documents/family/family.ttl");
+        m.write(System.out, "RDF/XML");
+    }
+    
+    public static void ontology() {
+        OntModel m = ModelFactory.createOntologyModel(OntModelSpec.OWL_MEM, null);
+        m.read("documents/test/owlDemoSchema.rdf");
+        m.write(System.out, "RDF/XML");
+    }
+
+    public static void printStatements(Model m, Resource s, Property p, Resource o) {
+        for (StmtIterator i = m.listStatements(s, p, o); i.hasNext();) {
+            Statement stmt = i.nextStatement();
+            System.out.println(" - " + PrintUtil.print(stmt));
+        }
+    }
+
+    public static void statement() {
+        // Model schema = RDFDataMgr.loadModel("documents/test/owlDemoSchema.rdf");
+        // Model data = RDFDataMgr.loadModel("documents/test/owlDemoData.rdf");
+        Model schema = RDFDataMgr.loadModel("documents/family/family.ttl");
+        Model data = RDFDataMgr.loadModel("documents/family/docs.ttl");
+        Reasoner reasoner = ReasonerRegistry.getOWLReasoner();
+        reasoner = reasoner.bindSchema(schema);
+        InfModel infoModel = ModelFactory.createInfModel(reasoner, data);
+
+        Resource nForce = infoModel.getResource("http://semweb.edu.vn/family#john");
+        System.out.println("nForce *:");
+        printStatements(infoModel, nForce, null, null);
+    }
     
     public static void main(String[] args) throws Exception {
-        // FusekiApp.connectFuseki("http://localhost:3030/database");
-        FusekiApp.createEmbeddedServer();
+        // App.ontology();
+        App.statement();
     }
 }
