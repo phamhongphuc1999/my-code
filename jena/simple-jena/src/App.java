ea34b77516d02ef6af5c9f05e48ff1c66c7a1f17
phamhongphuc1999 <phamhongphuc12atm1@gmail.com>
2023-09-06 20:20:47 +0700

    feat: rule

--- jena/simple-jena/src/App.java
+++ jena/simple-jena/src/App.java
@@ -1,15 +1,20 @@
+import java.util.List;
+
 import org.apache.jena.ontology.OntModel;
 import org.apache.jena.ontology.OntModelSpec;
 import org.apache.jena.rdf.model.InfModel;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
 import org.apache.jena.rdf.model.Property;
+import org.apache.jena.rdf.model.RDFNode;
 import org.apache.jena.rdf.model.ResIterator;
 import org.apache.jena.rdf.model.Resource;
 import org.apache.jena.rdf.model.Statement;
 import org.apache.jena.rdf.model.StmtIterator;
 import org.apache.jena.reasoner.Reasoner;
 import org.apache.jena.reasoner.ReasonerRegistry;
+import org.apache.jena.reasoner.rulesys.GenericRuleReasoner;
+import org.apache.jena.reasoner.rulesys.Rule;
 import org.apache.jena.riot.RDFDataMgr;
 import org.apache.jena.util.PrintUtil;
 import org.apache.jena.vocabulary.VCARD;
@@ -43,7 +48,7 @@ public class App {
     
     public static void ontology() {
         OntModel m = ModelFactory.createOntologyModel(OntModelSpec.OWL_MEM, null);
-        m.read("documents/test/owlDemoSchema.rdf");
+        m.read("documents/ontology.ttl");
         m.write(System.out, "RDF/XML");
     }
 
@@ -55,20 +60,33 @@ public class App {
     }
 
     public static void statement() {
-        // Model schema = RDFDataMgr.loadModel("documents/test/owlDemoSchema.rdf");
-        // Model data = RDFDataMgr.loadModel("documents/test/owlDemoData.rdf");
-        Model schema = RDFDataMgr.loadModel("documents/family/family.ttl");
-        Model data = RDFDataMgr.loadModel("documents/family/docs.ttl");
+        Model schema = RDFDataMgr.loadModel("documents/driver.ttl");
+        Model data = RDFDataMgr.loadModel("documents/driver.ttl");
         Reasoner reasoner = ReasonerRegistry.getOWLReasoner();
         reasoner = reasoner.bindSchema(schema);
         InfModel infoModel = ModelFactory.createInfModel(reasoner, data);
 
-        Resource nForce = infoModel.getResource("http://semweb.edu.vn/family#john");
+        Resource nForce = infoModel.getResource("http://example.org/NVA");
         System.out.println("nForce *:");
         printStatements(infoModel, nForce, null, null);
     }
+
+    public static void rule() {
+        Model rawData = RDFDataMgr.loadModel("documents/rule.ttl");
+        String ruleSrc = "@prefix : <http://semweb.edu.vn/rule-demo#> .\n" + //
+                "[rule1: (?a :p ?b) (?b :p ?c) -> (?a :p ?c)]";
+        List<Rule> rules = Rule.parseRules(ruleSrc);
+        Reasoner reasoner = new GenericRuleReasoner(rules);
+        InfModel inf = ModelFactory.createInfModel(reasoner, rawData);
+        System.out.println("A * * =>");
+        Resource nForce = inf.getResource("http://semweb.edu.vn/rule-demo#a");
+        StmtIterator list = inf.listStatements(nForce, null, (RDFNode)null);
+        while (list.hasNext()) {
+            System.out.println(" - " + list.next());
+        }
+    }
     
     public static void main(String[] args) throws Exception {
-        FusekiApp.ConnectTo();
+        App.rule();
     }
 }
