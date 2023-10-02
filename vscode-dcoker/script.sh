f5542a48cfbbc95ee89e5b15be9df016e97064cc
phamhongphuc1999 <phamhongphuc12atm1@gmail.com>
2023-10-02 16:33:42 +0700

    feat: test vscode docker

---
+++ vscode-dcoker/script.sh
@@ -0,0 +1,33 @@
+#! /bin/bash
+
+Green='\033[0;32m'
+NC='\033[0m'
+
+printf "${Green}Step 1: Update${NC}\n"
+apt -y update && apt -y upgrade
+
+printf "${Green}Step 2: Install sudo${NC}\n"
+apt -y install sudo
+printf "${Green}Step 2.1: Check sudo version${NC}\n"
+sudo --version
+
+printf "${Green}Step 3: Re-udpate${NC}\n"
+sudo apt update && sudo apt upgrade
+
+printf "${Green}Step 4: Install clang${NC}\n"
+sudo apt -y install clang
+
+printf "${Green}Step 4.1: Check clang version${NC}\n"
+clang --version
+
+printf "${Green}Step 5: Install vim${NC}\n"
+sudo apt -y install vim
+
+printf "${Green}Step 5.1: Check vim version${NC}\n"
+vim --version
+
+printf "${Green}Step 6: Install git${NC}\n"
+sudo apt -y install git
+
+printf "${Green}Step 6.1: Check git version${NC}\n"
+git --version
