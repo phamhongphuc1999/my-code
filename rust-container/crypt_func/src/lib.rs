98431f14847df101d93204b6eb0a1b5bb7254112
phamhongphuc1999 <phamhongphuc12atm1@gmail.com>
2025-08-30 22:31:24 +0700

    fix: crypt

--- rust-container/crypt_func/src/lib.rs
+++ rust-container/crypt_func/src/lib.rs
@@ -12,6 +12,13 @@ mod domain;
 
 pub use domain::*;
 
+const HEX_IV: [u8; 16] = [
+        0x00, 0x11, 0x22, 0x33,
+        0x44, 0x55, 0x66, 0x77,
+        0x88, 0x99, 0xaa, 0xbb,
+        0xcc, 0xdd, 0xee, 0xff,
+    ];
+
 #[wasm_bindgen(start)]
 pub fn main_js() {
     console_error_panic_hook::set_once();
@@ -38,7 +45,6 @@ fn _encrypt(key: &str, iv: &[u8], plain: &str) -> String {
     let key = derive_key(key);
 
     let mut buf = plain.as_bytes().to_vec();
-    // add capacity for padding up to next multiple of block size
     let bs = 16;
     let pad_len = bs - (buf.len() % bs);
     buf.extend(std::iter::repeat(0u8).take(pad_len));
@@ -50,16 +56,23 @@ fn _encrypt(key: &str, iv: &[u8], plain: &str) -> String {
     general_purpose::STANDARD.encode(ciphertext)
 }
 
-#[wasm_bindgen]
-pub fn encrypt(key: &str, iv: &[u8], plain: &str) -> String {
-    _encrypt(key, iv, plain)
+fn create_key(key: &str, iv: &[u8], timestamp: &str, uuid: &str) -> String {
+    check_domain();
+    let raw_result = format!("{}{}{}", timestamp, obfstr!("---"), uuid);
+    _encrypt(key, iv, &raw_result)
 }
 
-fn _decrypt(key: &str, iv: &[u8], cipher_b64: &str) -> String {
+#[wasm_bindgen]
+pub fn encrypt(timestamp: &str, uuid: &str, iv: &[u8], plain: &str) -> String {
+    let key = create_key(obfstr!("uXL5CwvBW3eZnWQX6uq5dcAT9wj0ZCMGv-onUe4zUfk"), &HEX_IV, timestamp, uuid);
+    _encrypt(&key, iv, plain)
+}
+
+fn _decrypt(key: &str, iv: &[u8], cipher_text: &str) -> String {
     check_domain();
     let key = derive_key(key);
 
-    let mut buf = general_purpose::STANDARD.decode(cipher_b64).unwrap();
+    let mut buf = general_purpose::STANDARD.decode(cipher_text).unwrap();
     let cipher = Decryptor::<Aes256>::new(&key.into(), iv.into());
     let decrypted = cipher.decrypt_padded_mut::<Pkcs7>(&mut buf).unwrap();
 
@@ -67,19 +80,9 @@ fn _decrypt(key: &str, iv: &[u8], cipher_b64: &str) -> String {
 }
 
 #[wasm_bindgen]
-pub fn decrypt(key: &str, iv: &[u8], cipher_b64: &str) -> String {
-    _decrypt(key, iv, cipher_b64)
-}
-
-fn _create_key(key: &str, iv: &[u8], timestamp: &str, uuid: &str) -> String {
-    check_domain();
-    let raw_result = format!("{}---{}", timestamp, uuid);
-    encrypt(key, iv, &raw_result)
-}
-
-#[wasm_bindgen]
-pub fn create_key(key: &str, iv: &[u8], timestamp: &str, uuid: &str) -> String {
-    _create_key(key, iv, timestamp, uuid)
+pub fn decrypt(timestamp: &str, uuid: &str, iv: &[u8], cipher_b64: &str) -> String {
+    let key = create_key(obfstr!("uXL5CwvBW3eZnWQX6uq5dcAT9wj0ZCMGv-onUe4zUfk"), &HEX_IV, timestamp, uuid);
+    _decrypt(&key, iv, cipher_b64)
 }
 
 #[cfg(test)]
@@ -87,35 +90,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_encrypt_decrypt() {
-        let key = "my secret key not 32 bytes"; // flexible size
-        let iv = [0u8; 16];
-        let msg = "Hello, AES flexible size key!";
-
-        let enc = encrypt(key, &iv, msg);
-        println!("cipher: {}", enc);
-
-        let dec = decrypt(key, &iv, &enc);
-        assert_eq!(msg, dec);
-    }
-
-    #[test]
-    fn test_create_key() {
-        let key = "my secret key not 32 bytes"; // flexible size
-        let iv = [0u8; 16];
-
-        let str1 = "1234567";
-        let str2 = "890";
-        let result = create_key(key, &iv, str1, str2);
-        let enc = encrypt(key, &iv, "1234567---890");
-
-        assert_eq!(result, enc);
-    }
-
-    #[test]
     fn test_flow() {
-        let key = "12345678";
-        // let iv = [0u8; 16];
         let iv = [
         0x00, 0x11, 0x22, 0x33,
         0x44, 0x55, 0x66, 0x77,
@@ -123,15 +98,12 @@ mod tests {
         0xcc, 0xdd, 0xee, 0xff,
     ];
 
-        let str1 = "1755533523";
-        let str2 = "ee05e338-5d0b-4bd6-99f6-41ab90eb1338";
-
-        let password = create_key(key, &iv, str1, str2);
-        println!("password: {}", password);
+        let timestamp = "1755533523";
+        let uuid = "ee05e338-5d0b-4bd6-99f6-41ab90eb1338";
 
         let msg = "Hello, AES flexible size key!";
-        let encrypted_msg = encrypt(&password, &iv, msg);
-        let decrypted_msg = decrypt(&password, &iv, &encrypted_msg);
+        let encrypted_msg = encrypt(&timestamp, &uuid, &iv, msg);
+        let decrypted_msg = decrypt(&timestamp, &uuid, &iv, &encrypted_msg);
         
         assert_eq!(decrypted_msg, msg);
     }
