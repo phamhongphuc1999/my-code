a36ce5d0171ced796566164b6ca51ef67b1c6189
phamhongphuc1999 <phamhongphuc12atm1@gmail.com>
2025-12-28 13:32:40 +0700

    new problems

--- c-language/problem48and40.cpp
+++ c-language/problem48and40.cpp
@@ -4,6 +4,8 @@
 #include <algorithm>
 #include <cmath>
 #include <unordered_map>
+#include <cstdint>
+#include <map>
 
 using namespace std;
 
@@ -75,51 +77,52 @@ vector<vector<int>> combinationSum2(vector<int> &candidates, int target)
   return result;
 }
 
-struct FourSumNode
-{
-  vector<int> arr;
-  string key;
-  int latestIndex;
-  int total;
-};
-
 // 18. 4Sum
 vector<vector<int>> fourSum(vector<int> &nums, int target)
 {
   vector<vector<int>> result;
-  if (nums.size() < 4)
+  int n = nums.size();
+  if (n < 4)
     return result;
+
   sort(nums.begin(), nums.end());
-  stack<FourSumNode> st;
-  unordered_map<string, bool> mp;
-  for (int i = 0; i < nums.size() - 3; i++)
+
+  for (int i = 0; i < n - 3; i++)
   {
-    string _key = to_string(nums[i]);
-    if (mp.count(_key) == 0)
-      st.push({{nums[i]}, _key, i, nums[i]});
-    mp.insert({_key, true});
-  }
-  while (st.size() > 0)
-  {
-    FourSumNode item = st.top();
-    st.pop();
-    if (item.arr.size() == 4)
+    if (i > 0 && nums[i] == nums[i - 1])
+      continue; // skip dup
+
+    for (int j = i + 1; j < n - 2; j++)
     {
-      if (item.total == target)
-        result.push_back(item.arr);
-    }
-    else if (item.arr.size() < 4)
-    {
-      for (int i = item.latestIndex + 1; i < nums.size(); i++)
+      if (j > i + 1 && nums[j] == nums[j - 1])
+        continue; // skip dup
+
+      long long rem = (long long)target - nums[i] - nums[j];
+      int l = j + 1, r = n - 1;
+
+      while (l < r)
       {
-        string _key = item.key + "_" + to_string(nums[i]);
-        if (mp.count(_key) == 0)
+        long long sum = nums[l] + nums[r];
+        if (sum == rem)
         {
-          vector<int> curr = item.arr;
-          curr.push_back(nums[i]);
-          int total = item.total + nums[i];
-          st.push({curr, _key, i, total});
-          mp.insert({_key, true});
+          result.push_back({nums[i], nums[j], nums[l], nums[r]});
+
+          // skip duplicates
+          while (l < r && nums[l] == nums[l + 1])
+            l++;
+          while (l < r && nums[r] == nums[r - 1])
+            r--;
+
+          l++;
+          r--;
+        }
+        else if (sum < rem)
+        {
+          l++;
+        }
+        else
+        {
+          r--;
         }
       }
     }
@@ -147,15 +150,96 @@ int countTriples(int n)
   return __result;
 }
 
+int countNegatives(vector<vector<int>> &grid)
+{
+  int m = grid.size();
+  int n = grid.at(0).size();
+  int row = m - 1;
+  int col = 0;
+  int count = 0;
+  while (row >= 0 && col < n)
+  {
+    if (grid.at(row).at(col) < 0)
+    {
+      count += n - col;
+      row -= 1;
+    }
+    else
+      col += 1;
+  }
+  return count;
+}
+
+int32_t clampToInt32(long long x)
+{
+  const long long INT32_MIN_LL = -2147483648LL;
+  const long long INT32_MAX_LL = 2147483647LL;
+
+  if (x < INT32_MIN_LL)
+    return INT32_MIN_LL;
+  if (x > INT32_MAX_LL)
+    return INT32_MAX_LL;
+  return static_cast<int32_t>(x);
+}
+
+// 8. String to Integer (atoi)
+int myAtoi(string s)
+{
+  int count__ = 0;
+  while (count__ < s.length())
+  {
+    if (s[count__] == ' ')
+      count__++;
+    else
+      break;
+  }
+  long long result = 0;
+  int count = 0;
+  int sign = 1;
+
+  while (count + count__ < s.length())
+  {
+    char _c = s.at(count + count__);
+    if (count == 0)
+    {
+      if (_c == '-')
+        sign = -1;
+      else if (_c != '+')
+      {
+        if (_c >= '0' && _c <= '9')
+        {
+          result = result * 10 + (_c - '0');
+        }
+        else
+          break;
+      }
+    }
+    else if (_c >= '0' && _c <= '9')
+    {
+      result = result * 10 + (_c - '0');
+      int _check__ = clampToInt32(result);
+      if (_check__ != result)
+        if (sign == 1)
+          return 2147483647;
+        else
+          return -2147483648;
+    }
+    else
+      break;
+    count++;
+  }
+  return result * sign;
+}
+
 int main()
 {
-  vector<int> a = {-5, 5, 4, -3, 0, 0, 4, -2};
-  vector<vector<int>> result = fourSum(a, 4);
+  vector<int> aaa{1, -2, -5, -4, -3, 3, 3, 5};
+  vector<vector<int>> result = fourSum(aaa, -11);
   for (vector<int> item : result)
   {
-    for (int value : item)
+    for (int i : item)
     {
-      cout << value << ", ";
+      cout << i << ", ";
     }
     cout << endl;
   }
