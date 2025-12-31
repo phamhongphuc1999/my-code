d6efe228347216b7026219d42132285a05ad31e7
phamhongphuc1999 <phamhongphuc12atm1@gmail.com>
2025-12-31 17:44:57 +0000

    create new problems

--- c-language/problem22.cpp
+++ c-language/problem22.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <bits/stdc++.h>
 
 using namespace std;
 
@@ -63,12 +64,177 @@ int maxWidthRamp(vector<int> &nums)
   return _max;
 }
 
+// problem 1970
+struct Node
+{
+  int row;
+  int column;
+  int direction; // 0-up, 1-down, 2-left, 3-right
+};
+
+class Solution
+{
+public:
+  bool canCross(int row, int col, vector<vector<int>> &cells, int day)
+  {
+    vector<vector<int>> grid(row, vector<int>(col, 1));
+
+    for (int i = 0; i <= day; i++)
+    {
+      grid[cells[i][0] - 1][cells[i][1] - 1] = 0;
+    }
+
+    queue<pair<int, int>> q;
+
+    for (int c = 0; c < col; c++)
+    {
+      if (grid[0][c] == 1)
+      {
+        q.push({0, c});
+        grid[0][c] = 2;
+      }
+    }
+
+    int dirs[4][2] = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+
+    while (!q.empty())
+    {
+      auto [r, c] = q.front();
+      q.pop();
+      if (r == row - 1)
+        return true;
+
+      for (auto &d : dirs)
+      {
+        int nr = r + d[0], nc = c + d[1];
+        if (nr >= 0 && nr < row && nc >= 0 && nc < col && grid[nr][nc] == 1)
+        {
+          grid[nr][nc] = 2;
+          q.push({nr, nc});
+        }
+      }
+    }
+
+    return false;
+  }
+
+  bool dfs(vector<vector<int>> &lands, int row, int col, vector<Node> &result)
+  {
+    if (result.size() == 0)
+    {
+      for (int i = 0; i < col; i++)
+      {
+        if (lands[0][i] == 1)
+        {
+          result.push_back({0, i, 0});
+          lands[0][i] = 2;
+          if (dfs(lands, row, col, result))
+            return true;
+          result.pop_back();
+          lands[0][i] = 1;
+        }
+      }
+      return false;
+    }
+    else
+    {
+      Node temp = result.back();
+      if (temp.row == row - 1)
+        return true;
+      if (temp.direction != 0 && temp.row > 0 && lands[temp.row - 1][temp.column] == 1)
+      {
+        result.push_back({temp.row - 1, temp.column, 1});
+        lands[temp.row - 1][temp.column] = 2;
+        if (dfs(lands, row, col, result))
+          return true;
+        result.pop_back();
+        lands[temp.row - 1][temp.column] = 1;
+      }
+      if (temp.direction != 1 && temp.row < row - 1 && lands[temp.row + 1][temp.column] == 1)
+      {
+        result.push_back({temp.row + 1, temp.column, 0});
+        lands[temp.row + 1][temp.column] = 2;
+        if (dfs(lands, row, col, result))
+          return true;
+        result.pop_back();
+        lands[temp.row + 1][temp.column] = 1;
+      }
+      if (temp.direction != 2 && temp.column > 0 && lands[temp.row][temp.column - 1] == 1)
+      {
+        result.push_back({temp.row, temp.column - 1, 3});
+        lands[temp.row][temp.column - 1] = 2;
+        if (dfs(lands, row, col, result))
+          return true;
+        result.pop_back();
+        lands[temp.row][temp.column - 1] = 1;
+      }
+      if (temp.direction != 3 && temp.column < col - 1 && lands[temp.row][temp.column + 1] == 1)
+      {
+        result.push_back({temp.row, temp.column + 1, 2});
+        lands[temp.row][temp.column + 1] = 2;
+        if (dfs(lands, row, col, result))
+          return true;
+        result.pop_back();
+        lands[temp.row][temp.column + 1] = 1;
+      }
+      return false;
+    }
+  }
+
+  vector<Node> findPath(int row, int col, vector<vector<int>> &cells, int index)
+  {
+    vector<vector<int>> lands(row, vector<int>(col, 1));
+
+    for (int i = 0; i <= index; i++)
+    {
+      int r = cells[i][0] - 1;
+      int c = cells[i][1] - 1;
+      lands[r][c] = 0;
+    }
+    vector<Node> result;
+    dfs(lands, row, col, result);
+    return result;
+  }
+  int latestDayToCross(int row, int col, vector<vector<int>> &cells)
+  {
+    int left = 0;
+    int right = cells.size() - 1;
+    int ans = -1;
+
+    while (left <= right)
+    {
+      int mid = left + (right - left) / 2;
+
+      if (canCross(row, col, cells, mid))
+      {
+        ans = mid;
+        left = mid + 1;
+      }
+      else
+      {
+        right = mid - 1;
+      }
+    }
+    return ans + 1;
+  }
+};
+
 int main()
 {
-  vector<string> result = generateParenthesis(1);
-  for (string i : result)
-  {
-    cout << i << endl;
-  }
-  return 0;
+  vector<vector<int>> cells = {
+      {1, 1}, {1, 2}, {2, 1}, {2, 2}};
+  Solution s = Solution();
+  int result = s.latestDayToCross(2, 2, cells);
+  cout << "latest day: " << result << endl;
+
+  // cout << "---------" << endl;
+  //   for (int i = 0; i < row; i++)
+  //   {
+  //     for (int j = 0; j < col; j++)
+  //     {
+  //       cout << lands[i][j] << ", ";
+  //     }
+  //     cout << endl;
+  //   }
+  //   cout << "---------" << endl;
 }
--- c-language/problem2554.cpp
+++ c-language/problem2554.cpp
@@ -38,8 +38,141 @@ int maxCount(vector<int> &banned, int n, int maxSum)
   return counter;
 }
 
+// problem 47
+class Solution47
+{
+public:
+  vector<vector<int>> permuteUnique(vector<int> &nums)
+  {
+    vector<vector<int>> results;
+    unordered_map<int, int> counters;
+    for (int num : nums)
+      counters[num]++;
+    vector<int> temp;
+    backtracking(temp, results, counters, 0, nums.size());
+    return results;
+  }
+
+  void backtracking(vector<int> temp, vector<vector<int>> &results, unordered_map<int, int> &counters, int index, int len)
+  {
+    if (index == len)
+      results.push_back(temp);
+    else
+    {
+
+      for (auto &item : counters)
+      {
+        if (item.second > 0)
+        {
+          temp.push_back(item.first);
+          item.second -= 1;
+          backtracking(temp, results, counters, index + 1, len);
+          temp.pop_back();
+          item.second += 1;
+        }
+      }
+    }
+  }
+};
+
+class Solution47_v2
+{
+public:
+  vector<vector<int>> permuteUnique(vector<int> &nums)
+  {
+    vector<vector<int>> results;
+    int counters[21];
+    memset(counters, 0, sizeof(counters));
+    for (int num : nums)
+      counters[num + 10]++;
+    vector<int> temp;
+    backtracking(temp, results, counters, 0, nums.size());
+    return results;
+  }
+
+  void backtracking(vector<int> temp, vector<vector<int>> &results, int counters[21], int index, int len)
+  {
+    if (index == len)
+      results.push_back(temp);
+    else
+    {
+      for (int i = 0; i < 21; i++)
+      {
+        if (counters[i] > 0)
+        {
+          temp.push_back(i - 10);
+          counters[i]--;
+          backtracking(temp, results, counters, index + 1, len);
+          temp.pop_back();
+          counters[i]++;
+        }
+      }
+    }
+  }
+};
+
+// problem 77
+class Solution77
+{
+public:
+  vector<vector<int>> combine(int n, int k)
+  {
+    vector<vector<int>> results;
+    int counters[n];
+    memset(counters, 1, sizeof(counters));
+    vector<int> temp;
+    backtracking(results, temp, counters, 0, k, n);
+    return results;
+  }
+
+  void backtracking(vector<vector<int>> &results, vector<int> &temp, int counters[], int index, int k, int n)
+  {
+    if (k == index)
+    {
+      results.push_back(temp);
+    }
+    else
+    {
+      for (int i = 1; i <= n; i++)
+      {
+        bool canRun = false;
+        int _size = temp.size();
+        if (_size == 0)
+        {
+          temp.push_back(i);
+          canRun = true;
+          counters[i - 1]--;
+        }
+        else
+        {
+          if (i + index - k <= n && i > temp[_size - 1])
+          {
+            temp.push_back(i);
+            canRun = true;
+            counters[i - 1]--;
+          }
+        }
+        if (canRun)
+        {
+          backtracking(results, temp, counters, index + 1, k, n);
+          temp.pop_back();
+          counters[i - 1]++;
+        }
+      }
+    }
+  }
+};
+
 int main()
 {
-  vector<int> vec1{1};
-  cout << maxCount(vec1, 10000, 1000000000) << endl;
+  Solution77 s = Solution77();
+  vector<vector<int>> results = s.combine(1, 1);
+  for (vector<int> item : results)
+  {
+    for (int i : item)
+    {
+      cout << i << ", ";
+    }
+    cout << endl;
+  }
 }
